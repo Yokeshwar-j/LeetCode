a1c76b542a5df17680a9c0cf919e8025c811ed78
Yokeshwar-j <yokeshwarj@gmail.com>
2026-02-28 13:15:15 +0530

    Time: 3 ms (91.58%) | Memory: 44.7 MB (56.90%) - LeetSync

---
+++ 242-valid-anagram/valid-anagram.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        int[] arr = new int[26];
+        for(char x : s.toCharArray()){
+            arr[x-'a']++;
+        }
+        for(char x : t.toCharArray()){
+            arr[x-'a']--;
+        }
+        int sum=0;
+        for(int x : arr){if(x!=0)return false;}
+        return true;
+    }
+}
