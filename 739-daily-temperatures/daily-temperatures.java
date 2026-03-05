9f133bf9370e7f7285fe49b4780cdd25798ab578
Yokeshwar-j <yokeshwarj@gmail.com>
2026-03-06 00:34:05 +0530

    Time: 71 ms (32.84%) | Memory: 107.5 MB (49.64%) - LeetSync

--- 739-daily-temperatures/daily-temperatures.java
+++ 739-daily-temperatures/daily-temperatures.java
@@ -3,7 +3,7 @@ class Solution {
         //Monotonic decreasing
         Stack<Integer> sm = new Stack<>();
         int[]arr = new int[t.length];
-        Arrays.fill(arr,0);
+        //Arrays.fill(arr,0);
         for(int i=0;i<t.length;i++){
             while(!sm.empty() && t[i]>t[sm.peek()]){
                 arr[sm.peek()]=i-sm.pop();
