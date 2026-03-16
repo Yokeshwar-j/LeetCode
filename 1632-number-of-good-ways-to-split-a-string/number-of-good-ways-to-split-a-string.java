3e912cfca927d5a0c741f0a707d3dd60de7b520f
Yokeshwar-j <yokeshwarj@gmail.com>
2026-03-16 21:07:00 +0530

    Time: 1750 ms (5.67%) | Memory: 48.5 MB (5.15%) - LeetSync

---
+++ 1632-number-of-good-ways-to-split-a-string/number-of-good-ways-to-split-a-string.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int numSplits(String s) {
+        HashMap<Character,Integer> hm = new HashMap<>();
+        for(char  p : s.toCharArray()){
+            hm.put(p,hm.getOrDefault(p,0)+1);
+        }
+        int c=0;
+        HashMap<Character,Integer> hh = new HashMap<>();   
+        for(char x : s.toCharArray()){
+            hh.put(x,hh.getOrDefault(x,0)+1);
+            System.out.println("hh->"+hh.get(x));
+            hm.put(x,hm.get(x)-1);
+            System.out.println("hm->"+hm.get(x));
+            if(hm.get(x)==0)hm.remove(x);
+            if(hm.size()==hh.size())c++;
+            System.out.println("****");
+        }
+        return c;
+    }
+}
