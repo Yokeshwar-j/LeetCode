c026d74f5eb3de7d7799e3c71290dd06711f7dfa
Yokeshwar-j <yokeshwarj@gmail.com>
2026-03-19 23:42:23 +0530

    Time: 2 ms (99.79%) | Memory: 53.6 MB (64.68%) - LeetSync

---
+++ 1039-find-the-town-judge/find-the-town-judge.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int findJudge(int n, int[][] trust) {
+        int[] arr = new int[n+1];
+        for(int[] i : trust){
+            arr[i[1]]++;arr[i[0]]--;
+        }
+        for(int i=1;i<n+1;i++){if(arr[i]==(n-1))return i;}
+        return -1;
+    }
+}
