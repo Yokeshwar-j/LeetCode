1052bf39c18216f7a0a836ca0514381b39ebad16
Yokeshwar-j <yokeshwarj@gmail.com>
2026-03-20 00:56:54 +0530

    Time: 1196 ms (5.00%) | Memory: 163.1 MB (5.05%) - LeetSync

---
+++ 239-sliding-window-maximum/sliding-window-maximum.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int[] maxSlidingWindow(int[] nums, int k) {
+        //using a  monotonic deque
+        Deque<Integer> dq = new ArrayDeque<>();
+        List<Integer> ls= new ArrayList<>();
+        int i=0,j=0;
+        while(j<nums.length){
+            while(!dq.isEmpty() && dq.getLast()<nums[j]){dq.removeLast();}
+            dq.addLast(nums[j]);
+            if(j-i+1==k && !dq.isEmpty()){
+                System.out.println("Current High --> "+dq.getFirst());
+                ls.add(dq.getFirst());
+                if(nums[i++]==dq.getFirst())dq.removeFirst();
+            }
+            j++;
+        }
+        int[]arr = new int[ls.size()];i=0;
+        for(Integer x : ls){arr[i++]=x;}
+        return arr;
+    }
+}
