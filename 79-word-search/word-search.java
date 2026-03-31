41a4167ecc437e5e946f7e63a77ac20e8487ecd8
Yokeshwar-j <yokeshwarj@gmail.com>
2026-03-31 20:15:11 +0530

    Time: 102 ms (94.88%) | Memory: 43.2 MB (37.26%) - LeetSync

--- 79-word-search/word-search.java
+++ 79-word-search/word-search.java
@@ -24,7 +24,7 @@ class Solution {
         if(j+1< bd[0].length && bd[i][j+1]==p.charAt(ind))c1 = dfs(bd,p,i,j+1,ind+1);
         if(j-1>=0 && bd[i][j-1]==p.charAt(ind))c2= dfs(bd,p,i,j-1,ind+1);
         if(i-1>=0 && bd[i-1][j]==p.charAt(ind))c3= dfs(bd,p,i-1,j,ind+1);
-        if(i+1 < bd.length && bd[i+1][j]==p.charAt(ind)){System.out.println("in");c4 =dfs(bd,p,i+1,j,ind+1);}
+        if(i+1 < bd.length && bd[i+1][j]==p.charAt(ind))c4 =dfs(bd,p,i+1,j,ind+1);
         bd[i][j]=temp;
         return c1 || c2 || c3 || c4;
     }
