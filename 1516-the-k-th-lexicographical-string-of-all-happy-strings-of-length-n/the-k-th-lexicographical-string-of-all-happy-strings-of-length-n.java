9a038c74146445c318e301988847a4a0b269d084
Yokeshwar J <yokeshwarj@gmail.com>
2026-04-10 22:44:41 +0530

    Time: 987 ms (5.44%) | Memory: 46.6 MB (37.66%) - LeetSync

---
+++ 1516-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/the-k-th-lexicographical-string-of-all-happy-strings-of-length-n.java
@@ -0,0 +1,28 @@
+class Solution {
+    char[] arr = new char[]{'a','b','c'};
+    public String getHappyString(int n, int k) {
+        //List<Character> ls= new ArrayList<>();
+        
+        int i=0,len=n;
+        /**while(n!=0){
+            ls.add(arr[(i++)%len]);
+        }*/
+        List<String> fin = new ArrayList<>();
+        StringBuilder sb = new StringBuilder();
+        back(fin,len,sb,'t');
+        return fin.size()>=k?fin.get(k-1):"";
+    }
+    public void back(List<String>fin,int len,StringBuilder sb,char last){
+        if(sb.length()==len){System.out.println(sb);fin.add(sb.toString());return;}
+        for(int i=0;i<3;i++){
+            if(last==arr[i]){System.out.println("hi");continue;}
+            char temp = last;
+            last=arr[i];
+            sb.append(arr[i]);
+            System.out.println("Push->"+sb);
+            back(fin,len,sb,last);
+            last=temp;
+            sb.deleteCharAt(sb.length()-1);
+        }
+    }
+}
