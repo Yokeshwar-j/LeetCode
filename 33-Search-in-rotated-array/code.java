6860657e40a6c2bf2b3079cccff77e88ac842293
Yokeshwar J <yokeshwarj@gmail.com>
2026-08-07 12:09:33 +0530

    Implement search method for rotated sorted array

---
+++ 33-Search-in-rotated-array/code.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int search(int[] nums, int target) {
+        int i=0,j=nums.length-1;
+        while(i<=j){
+            if(nums[i]==target){return i;}
+            else if(nums[j]==target){return j;}
+            i++;j--;
+        }
+        return -1;
+    }
+}
